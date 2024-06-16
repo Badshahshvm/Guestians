71b03cb6b46aa700514e2abfd6f9a37ae657f318
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2024-06-16 18:15:24 +0530

    review functionality is added

--- models/listing.js
+++ models/listing.js
@@ -1,29 +1,34 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const reviewModel = require("./review");
 
-const listSchema = mongoose.Schema({
+const listSchema = Schema({
   title: {
     type: String,
-    reuired: true,
+    required: true, // corrected typo: changed 'reuired' to 'required'
   },
   description: String,
   image: {
     type: String,
-    default:
-      "https://www.google.com/imgres?q=hotel&imgurl=https%3A%2F%2Fwww.kayak.co.in%2Fnews%2Fwp-content%2Fuploads%2Fsites%2F76%2F2023%2F08%2FTHEME_HOTEL_SIGN_FIVE_STARS_FACADE_BUILDING_GettyImages-1320779330-3.jpg&imgrefurl=https%3A%2F%2Fwww.kayak.co.in%2Fnews%2Fwhat-do-hotel-stars-mean%2F&docid=j4uvNRhAPYcEeM&tbnid=4-4RPo13QrA8FM&vet=12ahUKEwi10vLX4NiGAxXCcmwGHTcyCk4QM3oECGcQAA..i&w=1920&h=1312&hcb=2&ved=2ahUKEwi10vLX4NiGAxXCcmwGHTcyCk4QM3oECGcQAA",
-    set: (v) =>
-      v === ""
-        ? "https://www.google.com/imgres?q=hotel&imgurl=https%3A%2F%2Fwww.kayak.co.in%2Fnews%2Fwp-content%2Fuploads%2Fsites%2F76%2F2023%2F08%2FTHEME_HOTEL_SIGN_FIVE_STARS_FACADE_BUILDING_GettyImages-1320779330-3.jpg&imgrefurl=https%3A%2F%2Fwww.kayak.co.in%2Fnews%2Fwhat-do-hotel-stars-mean%2F&docid=j4uvNRhAPYcEeM&tbnid=4-4RPo13QrA8FM&vet=12ahUKEwi10vLX4NiGAxXCcmwGHTcyCk4QM3oECGcQAA..i&w=1920&h=1312&hcb=2&ved=2ahUKEwi10vLX4NiGAxXCcmwGHTcyCk4QM3oECGcQAA"
-        : v,
+    default: "https://www.example.com/default-image.jpg", // simplified default image URL
+    set: (v) => (v ? v : "https://www.example.com/default-image.jpg"), // simplified setter
   },
   price: Number,
   location: String,
   country: String,
   reviews: [
     {
-      type: mongoose.Schema.Types.ObjectId,
+      type: Schema.Types.ObjectId,
       ref: "Review",
     },
   ],
 });
-const listModel = mongoose.model("Listing", listSchema);
-module.exports = listModel;
+
+listSchema.post("findOneAndDelete", async function (listing) {
+  if (listing) {
+    await reviewModel.deleteMany({ _id: { $in: listing.reviews } }).exec();
+  }
+});
+
+const Listing = mongoose.model("Listing", listSchema); // corrected model name to Listing
+module.exports = Listing; // exporting Listing model
